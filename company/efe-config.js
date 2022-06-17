70476314111c49df8f824ab25ef2cce4191415d7
hollow <1315742953@qq.com>
2022-06-17 16:41:06 +0800

    接口迁移

---
+++ company/efe-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  webCdnUrl: "E:\\web-cdn\\tn\\activity\\twjsm\\gash", // 连接符需要用\\，本次活动需求放置的本地web-cdn目录的绝对路径，如：E:\\efun-project\\web-cdn\\en\\test\\copy
+  pcPreFtpUrl: "/gwtn/twjsm/pctest/event/apiMigrate", // 本次活动pc预发布ftp静态服务器路径，如：/gw/smwm/event/pre
+  pcFtpUrl: "/gwtn/twjsm/pc/event/apiMigrate", // 本次活动pc正式ftp静态服务器路径，如：/gw/smwm/event/pre
+  mbPreFtpUrl: "/gwtn/twjsm/mobiletest/event/apiMigrate", // 本次活动mb预发布ftp静态服务器路径，如：/gw/smwm/event/pre
+  mbFtpUrl: "/gwtn/twjsm/mobile/event/apiMigrate", // 本次活动mb正式ftp静态服务器路径，如：/gw/smwm/event/pre
+};
