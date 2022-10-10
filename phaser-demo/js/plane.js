ad415007d4123f885b902b7f73bbf1b80e6d2f67
illusion <1315742953@qq.com>
2022-10-11 00:26:50 +0800

    1

--- phaser-demo/js/plane.js
+++ phaser-demo/js/plane.js
@@ -43,10 +43,84 @@ game.States.preload = function () {
     }
 
     this.create = function () {
+        game.state.start('play')
+    }
+}
+
+game.States.start = function () {
+    this.create = function () {
         game.add.image(0, 0, 'bg')
+        game.add.image(game.world.centerX, game.world.height - 10, 'copyright').anchor.set(0.5, 0.5)
+        const plane = game.add.sprite(game.world.centerX, game.world.centerY, 'myplane')
+        plane.anchor.set(0.5, 0.5)
+        plane.animations.add('fly')
+        plane.animations.play('fly', 15, true)
+        game.add.button(game.world.centerX, (game.world.height / 2) + 50, 'startbutton', this.onStartClick, this, 1, 0, 1, 0).anchor.set(0.5, 0.5)
+    }
+    this.onStartClick = function () {
+        game.state.start('play')
+    }
+}
+
+game.States.play = function () {
+    let plane;
+    let text;
+    let myBulletGroup;
+    let lastTime = new Date().getTime();
+    let enemyGroup;
+    let num = 0;
+    this.create = function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        const bg = game.add.tileSprite(0, 0, game.width, game.height, 'bg')
+        bg.autoScroll(0, 20);
+        plane = game.add.sprite(game.world.centerX, game.world.centerY, 'myplane')
+        plane.anchor.set(0.5, 0.5)
+        plane.animations.add('fly')
+        plane.animations.play('fly', 15, true)
+        const tween = game.add.tween(plane).to({
+            y: game.world.height - 20
+        }, 1000, Phaser.Easing.Sinusoidal.Out, true);
+        tween.onComplete.add(onStart);
+        enemyGroup = game.add.group();
+        enemyGroup.enableBody = true;
+    }
+
+    this.update = function () {
+        const dis = new Date().getTime() - lastTime;
+        if (plane.isFire && dis > 500) {
+            const bullet = myBulletGroup.create(plane.x, plane.y, 'bullet')
+            const enemy = enemyGroup.create(Math.random() * game.world.width, 0, 'enemy1')
+            enemy.body.velocity.y = 10;
+            bullet.body.velocity.y = -200;
+            lastTime = new Date().getTime();
+        }
+
+        game.physics.arcade.overlap(enemyGroup, myBulletGroup, colliderCallback)
+    }
+
+    function colliderCallback(bullet, enemy) {
+        bullet.kill();
+        enemy.kill();
+        text.text = 'Score: ' + (num += 10);
+    }
+
+    function onStart() {
+        plane.inputEnabled = true;
+        plane.input.enableDrag(true);
+        text = game.add.text(0, 0, 'Score: 0', {
+            font: '16px',
+            fill: '#000'
+        });
+        plane.isFire = true;
+        game.physics.enable(plane, Phaser.Physics.ARCADE)
+        plane.body.colliderWorldBounds = true;
+        myBulletGroup = game.add.group();
+        myBulletGroup.enableBody = true;
     }
 }
 
 game.state.add('boot', game.States.boot)
 game.state.add('preload', game.States.preload)
+game.state.add('start', game.States.start)
+game.state.add('play', game.States.play)
 game.state.start('boot')
